630d3a878635ad640d3a9f07ea7d62ae851c723c
GeroCoded <luisfergeronimo@gmail.com>
2019-10-14 22:06:20 -0500

    Funciones con function a funciones de flecha

--- sockets/socket-servidor.js
+++ sockets/socket-servidor.js
@@ -8,19 +8,19 @@ io.on('connection', (client) => {
 	console.log('Usuario Conectado');
 
 
-	client.on('join', function(data){
+	client.on('join', (data) => {
 		client.join(data.sala);
 		console.log(data.user + ' ingreso a la sala: ' + data.sala);
 		client.broadcast.to(data.sala).emit('nuevoUsuario ', {user:data.user, mensaje: ' ha ingresado a esta sala'});
 	});
 
-	client.on('leave', function(data){
+	client.on('leave', (data) => {
 		console.log(data.user + ' dejó la sala: ' + data.sala);
 		client.broadcast.to(data.sala).emit('Un usuario a abandonado la sala ', {user:data.user, mensaje:' abandonó la sala'});
 		client.leave(data.sala);
 	});
 
-	client.on('nuevoMensaje',function(data){
+	client.on('nuevoMensaje', (data) => {
 		console.log('Información recibida: ', data);
 		io.in(data.sala).emit('Respuesta', { mensaje: data })
 	});
